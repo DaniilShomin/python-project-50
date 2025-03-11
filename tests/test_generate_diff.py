9651f1d626d2fed1356aab51f2fe32e9023a1a17
DaniilShomin <d.sh.v@yandex.ru>
2025-03-11 20:44:17 +0300

    add new tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -2,13 +2,13 @@ from gendiff.modules import generate_diff
 
 
 def test_generate_diff():
-    result = '''{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}'''
+    # flat diff
+    with open('tests/test_data/flat_result.txt') as f:
+        result = f.read()
     assert generate_diff('data/file1.json', 'data/file2.json') == result
-    assert generate_diff('data/file1.yaml', 'data/file2.yaml', 'yaml') == result
+    assert generate_diff('data/file1.yaml', 'data/file2.yaml') == result
+    # nested diff
+    with open('tests/test_data/nested_result.txt') as f:
+        result = f.read()
+    assert generate_diff('data/file3.json', 'data/file4.json') == result
+    assert generate_diff('data/file3.yaml', 'data/file4.yaml') == result
