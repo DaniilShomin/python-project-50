0fdcd5aab44d193721187f1e3b2f83be1f3467d7
DaniilShomin <d.sh.v@yandex.ru>
2025-03-15 14:54:25 +0300

    fix bagg output

--- gendiff/diff_json.py
+++ gendiff/diff_json.py
@@ -4,23 +4,22 @@ import json
 
 def get_generated_diff_json(data1, data2):  # noqa: C901
     copy_file2 = copy.deepcopy(data2)
-    
+
     def iter(current_value, current_value2):  # noqa: C901
         result = {}
         for key, val in current_value.items():
             if not isinstance(val, dict):
                 if current_value2.get(key, 'not_key') != 'not_key':
                     if current_value[key] == current_value2[key]:
-                        result[key] = current_value2.pop(key)                  
+                        result[key] = current_value2.pop(key)
             else:
                 if current_value2.get(key, 'not_key') != 'not_key':
                     if not isinstance(current_value2[key], str):
                         result[key] = iter(current_value[key], 
-                                           current_value2[key])
+                                           current_value2.pop(key))
             if list(current_value)[-1] == key:
                 if len(current_value2) > 0:
                     for key, val in current_value2.items():
                         result[key] = val
-        result = json.dumps(result, indent=2, sort_keys=True)
         return result
-    return iter(data1, copy_file2)
+    return json.dumps(iter(data1, copy_file2), indent=2, sort_keys=True)
