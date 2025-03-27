2fac791ad67b97e25ae10b0e47bc1762efb934ac
DaniilShomin <d.sh.v@yandex.ru>
2025-03-28 00:39:38 +0300

    change logic

--- gendiff/diff_plain.py
+++ gendiff/diff_plain.py
@@ -1,47 +1,69 @@
 import itertools
-import json
+
+from gendiff.interface import (
+    get_action,
+    get_key,
+    get_value,
+    get_value_after,
+    get_value_before,
+    is_nested,
+)
 
 
-def plain(value):  # noqa: C901
-
-    def iter_(current_value, pre_key=''):  # noqa: C901
-        if not isinstance(current_value, list):
-            return json.dumps(current_value)
+def plain(data):  # noqa C901
+    def iter_(value, pre_key=""):  # noqa C901
         
-        if pre_key == "complex":
-            return "[complex value]"
-
         lines = []
-        for item in current_value:
-            if not pre_key:
-                if item["type"] == "saved":
-                    lines.append(
-                        iter_(item["value"], f"Property '{item["key"]}"))
-                elif item["type"] == "removed":
-                    lines.append(f"Property '{item["key"]}' was removed")
-                elif item["type"] == "added":
-                    lines.append(
-                        f"Property '{item["key"]}' was added with value: "
-                        f"{iter_(item["value"], "complex")}")
-            else:
-                if item["type"] == "removed":
-                    lines.append(f"{pre_key}.{item["key"]}' was removed")
-                elif item["type"] == "changed":
-                    lines.append(
-                        f"{pre_key}.{item["key"]}' was updated. From "
-                        f"{iter_(item["value"][0], "complex")} to "
-                        f"{iter_(item["value"][1], "complex")}")
-                elif item["type"] == "added":
-                    lines.append(
-                        f"{pre_key}.{item["key"]}' was added with value: "
-                        f"{iter_(item["value"], "complex")}")
-                elif item["type"] == "saved" \
-                    and isinstance(item["value"], list):
-                    lines.append(
-                        iter_(item["value"], f"{pre_key}.{item["key"]}"))
 
+        for item in value:
+            if not pre_key:
+                match get_action(item):
+                    case 'saved':
+                        lines.append(iter_(get_value(item),
+                                           f"Property '{get_key(item)}"))
+                    case 'added':
+                        lines.append(
+                          f"Property '{get_key(item)}' was added with value: "
+                          f"{get_value(item) if not is_nested(get_value(item))
+                             else "[complex value]"}"
+                        )
+                    case 'removed':
+                        lines.append(f"Property '{get_key(item)}' was removed")
+                    case 'changed':
+                        lines.append(
+                            f"Property '{get_key(item)}' was updated. From "
+                            f"{get_value_before(item) 
+                               if not is_nested(get_value_before(item))
+                               else "[complex value]"} to "
+                            f"{get_value_after(item) 
+                               if not is_nested(get_value_after(item))
+                               else "[complex value]"}"
+                        )
+            else:
+                match get_action(item):
+                    case 'saved':
+                        if is_nested(get_value(item)):
+                            lines.append(iter_(get_value(item), 
+                                               f"{pre_key}.{get_key(item)}"))
+                    case 'added':
+                        lines.append(
+                          f"{pre_key}.{get_key(item)}' was added with value: "
+                          f"{get_value(item) if not is_nested(get_value(item))
+                                           else "[complex value]"}"
+                        )
+                    case 'removed':
+                        lines.append(f"{pre_key}.{get_key(item)}' was removed")
+                    case 'changed':
+                        lines.append(
+                            f"{pre_key}.{get_key(item)}' was updated. From "
+                            f"{get_value_before(item) 
+                               if not is_nested(get_value_before(item)) 
+                               else "[complex value]"} to "
+                            f"{get_value_after(item) 
+                               if not is_nested(get_value_after(item)) 
+                               else "[complex value]"}"
+                        )
             lines = sorted(lines, key=lambda item: item.split()[1])
         result = itertools.chain(lines)    
         return '\n'.join(result)
-
-    return iter_(value, '').replace('"', "'")
+    return iter_(data, '')
--- gendiff/diff_stylish.py
+++ gendiff/diff_stylish.py
@@ -1,43 +1,52 @@
 import itertools
-import json
+
+from gendiff.interface import (
+    get_action,
+    get_key,
+    get_value,
+    get_value_after,
+    get_value_before,
+    is_nested,
+)
 
 
 def pref():
     return ['  ', '- ', '+ ']  
 
 
-def stylish(value, count=1):
+def stylish(data, count=1):
     prefix = pref()
 
-    def iter_(current_value, depth):
-        if not isinstance(current_value, list):
-            return json.dumps(current_value)
+    def iter_(value, depth):
+        if not is_nested(value):
+            return get_value(value)
         
         indent_size = depth + count
         deep_indent = indent_size + 1
         indent = indent_size * '  ' 
         current_indent = '  ' * depth
         lines = []
-        for item in current_value:
-            if item["type"] == "saved":
-                lines.append(
-                    f'{indent}{prefix[0]}{item["key"]}: '
-                    f'{iter_(item["value"], deep_indent)}')
-            elif item["type"] == "removed":
-                lines.append(
-                    f'{indent}{prefix[1]}{item["key"]}: '
-                    f'{iter_(item["value"], deep_indent)}')
-            elif item["type"] == "changed":
-                lines.append(
-                    f'{indent}{prefix[1]}{item["key"]}: '
-                    f'{iter_(item["value"][0], deep_indent)}')
-                lines.append(
-                    f'{indent}{prefix[2]}{item["key"]}: '
-                    f'{iter_(item["value"][1], deep_indent)}')
-            elif item["type"] == "added":
-                lines.append(
-                    f'{indent}{prefix[2]}{item["key"]}: '
-                    f'{iter_(item["value"], deep_indent)}')
+        for item in value:
+            match get_action(item):
+                case 'saved':
+                    lines.append(
+                        f'{indent}{prefix[0]}{get_key(item)}: '
+                        f'{iter_(get_value(item), deep_indent)}')
+                case 'removed':
+                    lines.append(
+                        f'{indent}{prefix[1]}{get_key(item)}: '
+                        f'{iter_(get_value(item), deep_indent)}')
+                case 'changed':
+                    lines.append(
+                        f'{indent}{prefix[1]}{get_key(item)}: '
+                        f'{iter_(get_value_before(item), deep_indent)}')
+                    lines.append(
+                        f'{indent}{prefix[2]}{get_key(item)}: '
+                        f'{iter_(get_value_after(item), deep_indent)}')
+                case 'added':
+                    lines.append(
+                        f'{indent}{prefix[2]}{get_key(item)}: '
+                        f'{iter_(get_value(item), deep_indent)}')
             lines = sorted(
                 lines, 
                 key=lambda item: item.split(f'{indent}')[1].split(':')[0][2:]
@@ -47,4 +56,4 @@ def stylish(value, count=1):
         result = itertools.chain("{", lines, [current_indent + "}"])
         return '\n'.join(result)
     
-    return iter_(value, 0).replace('"', '')
+    return iter_(data, 0).replace("'", '')
